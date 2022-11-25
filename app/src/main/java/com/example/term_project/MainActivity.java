b38d7f87d67be8481dbcb60d56854c6528775150
whdms <whdms1107@naver.com>
2022-11-25 23:01:52 +0900

    yes!!

--- app/src/main/java/com/example/term_project/MainActivity.java
+++ app/src/main/java/com/example/term_project/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.term_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = findViewById(R.id.spinner);
+        TextView textView;
+        textView = findViewById(R.id.textView);
+        String[] items = {"천안", "신관", "예산"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                //API에 만들어져 있는 R.layout.simple_spinner...를 씀
+                this,android.R.layout.simple_spinner_item, items
+        );
+        //미리 정의된 레이아웃 사용
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        // 스피너 객체에다가 어댑터를 넣어줌
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            // 선택되면
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText(items[position]);
+            }
+
+            // 아무것도 선택되지 않은 상태일 때
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                textView.setText("선택: ");
+            }
+        });
     }
 }
